52a24ca892b601c036f230773736febcaf97aa93
Shourov98 <mdmahbub0412@gmail.com>
2024-10-11 18:43:18 +0600

    Appwrite Services Athentication and appwrite environment setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
   return (
     <>
       <h1>Mega Blog with Appwrite</h1>
