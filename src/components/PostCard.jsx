cab26e5704dd0506685f02947e40bb15289d4c26
Shourov98 <mdmahbub0412@gmail.com>
2024-10-12 16:54:04 +0600

    Good looking of Home Page

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,22 +3,21 @@ import appwriteService from "../appwrite/config"
 import {Link} from 'react-router-dom'
 
 function PostCard({$id, title, featuredImage}) {
-    
   return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4'>
-            <div className='w-full justify-center mb-4'>
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-                className='rounded-xl' />
-
-            </div>
-            <h2
-            className='text-xl font-bold'
-            >{title}</h2>
+      <div className='w-full bg-gray-100 rounded-xl p-4 shadow-md'>
+        <div className='w-full mb-4'>
+          {/* Maintain 16:9 aspect ratio for the image */}
+          <img 
+            src={appwriteService.getFilePreview(featuredImage)} 
+            alt={title}
+            className='rounded-xl w-full h-[200px] object-cover' 
+          />
         </div>
+        <h2 className='text-xl font-bold text-center'>{title}</h2>
+      </div>
     </Link>
   )
 }
-
 
 export default PostCard
